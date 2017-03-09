a61619e357a68fbe19e9c4cfb3be3d10837ae9bb
Jenn Tustin <jenn.tustin@gmail.com>
2017-03-09 12:19:45 -0500

    Fix associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,13 @@
 class Recipe < ApplicationRecord
 
   belongs_to :user
-  
-  has_many :ingredients
-  has_many :directions
 
-  accepts_nested_attributes_for :ingredients,
-    reject_if: proc { |attributes| attributes['name'].blank? },
-    allow_destroy: true
+  has_many :ingredients, inverse_of: :recipe
+  has_many :directions, inverse_of: :recipe
 
-  accepts_nested_attributes_for :directions,
-    reject_if: proc { |attributes| attributes['steps'].blank? },
-    allow_destroy: true
+  accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
+
+  accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['steps'].blank? }, allow_destroy: true
 
   validates :title, :description, :image, presence: true
 
